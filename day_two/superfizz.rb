dc1354b1a2a3e27ec428a9ebdbe407b9a9a12495
Amber Crawford <ammcrawford@gmail.com>
2015-08-25 20:14:28 -0600

    update superfizz commit

--- day_two/superfizz.rb
+++ day_two/superfizz.rb
@@ -1,21 +1,21 @@
-
-# 1001.times do |number|
-#   if (number % 3 == 0) && (number % 5 == 0) && (number % 7 == 0)
-#     puts "SuperFizzBuzz"
-#   elsif (number % 3 == 0) && (number % 7 == 0)
-#     puts "SuperFizz"
-#   elsif (number % 5 == 0) && (number % 7 == 0)
-#     puts "SuperBuzz"
-#   elsif (number % 3 == 0)
-#     puts "Fizz"
-#   elsif (number % 5 == 0)
-#     puts "Buzz"
-#   elsif (number % 7 == 0)
-#     puts "Super"
-#   else
-#     number
-#   end
-# end
+# Version 1
+ 1001.times do |number|
+  if (number % 3 == 0) && (number % 5 == 0) && (number % 7 == 0)
+    puts "SuperFizzBuzz"
+  elsif (number % 3 == 0) && (number % 7 == 0)
+    puts "SuperFizz"
+  elsif (number % 5 == 0) && (number % 7 == 0)
+    puts "SuperBuzz"
+  elsif (number % 3 == 0)
+    puts "Fizz"
+  elsif (number % 5 == 0)
+    puts "Buzz"
+  elsif (number % 7 == 0)
+    puts "Super"
+  else
+    number
+  end
+end
 
 # OR
 
